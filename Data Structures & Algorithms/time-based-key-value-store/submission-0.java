000b5e1484f8854feed9fad9959e6b0c78aba267
Pavithra-Project <pavithrav811@gmail.com>
2026-08-07 19:32:10 +0530

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.java
@@ -0,0 +1,45 @@
+class TimeMap {
+
+    class Pair{
+        String value;
+        int timestamp;
+        Pair(String value,int timestamp)
+        {
+            this.value=value;
+            this.timestamp=timestamp;
+        }
+    }
+    HashMap<String,ArrayList<Pair>>map;
+    public TimeMap() {
+        map=new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if(!map.containsKey(key))
+        map.put(key,new ArrayList<>());
+        map.get(key).add(new Pair(value,timestamp));
+    }
+    
+    public String get(String key, int timestamp) {
+        if(!map.containsKey(key))
+        return "";
+        ArrayList<Pair>list=map.get(key);
+        String ans="";
+        int left=0,right=list.size()-1;
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if(list.get(mid).timestamp<=timestamp)
+            {
+                ans=list.get(mid).value;
+                left=mid+1;
+            }
+            else
+            {
+                right=mid-1;
+
+            }
+        }
+        return ans;
+    }
+}
